e1845cd22a395e8d4919195d96a6e3dc7ec1597c
nikolaivasilev12 <nikolaivasilev208@gmail.com>
2020-10-21 11:15:12 +0200

    Finished order

--- Controllers/Order.php
+++ Controllers/Order.php
@@ -6,19 +6,25 @@ class Order extends Controller
     function saveOrder($order, $cartItem)
     {
 
-        self::query("INSERT INTO `order` (customerID) VALUES ('{$order['customerID']}')");
+        self::query("INSERT INTO `order` (customerID) VALUES ( ? ) ", array($order['customerID']));
         $orderID = self::query("SELECT * FROM `order` ORDER BY orderID DESC LIMIT 1");
 
         foreach ($cartItem as $row => $innerArray) {
+            $orderHasProductParams = array($innerArray['price'], $innerArray['code'], $innerArray['quantity'], $orderID[0]['orderID']);
             self::query("INSERT INTO `orderhasproduct` (price, `productID`, `amount`, `orderID`) 
-            VALUES ('{$innerArray['price']}', '{$innerArray['code']}', '{$innerArray['quantity']}', '{$orderID[0]['orderID']}');");
+            VALUES ( ? , ? , ? , ? )", $orderHasProductParams);
         };
-
-        $postalCodeExists = self::query("SELECT 1 FROM postalcode WHERE postalCode = '{$order['zipcode']}'");
-
+        
+        $postalCodeExists = self::query("SELECT 1 FROM postalcode WHERE postalCode = ? ", array($order['zipcode']));
+        
         if (!!$postalCodeExists !== true) {
-            self::query("INSERT INTO postalcode (postalCode, city) VALUES ('{$order['zipcode']}', '{$order['city']}')");
+            $postalCodeParams = array($order['zipcode'], $order['city']);
+            self::query("INSERT INTO postalcode (postalCode, city) VALUES ( ? , ? )", $postalCodeParams);
         }
-        self::query("INSERT INTO adress (street, postalCode) VALUES ('{$order['adress']}', '{$order['zipcode']}')");
+        $adressParams = array($order['adress'], $order['zipcode']);
+        self::query("INSERT INTO adress (street, postalCode) VALUES ( ? , ? )", $adressParams);
+        
+        $getLastAddress = $this->array_flatten(self::query("SELECT * FROM adress ORDER BY adressID DESC LIMIT 1"));
+        self::query("UPDATE `order` SET adressID = ? WHERE orderID = ?", array($getLastAddress['adressID'], $orderID[0]['orderID']));
     }
 }
