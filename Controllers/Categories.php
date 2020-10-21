4fdf345beec3d4aec245c8d1341cd31abc8c0cb3
nikolaivasilev12 <nikolaivasilev208@gmail.com>
2020-10-21 10:21:56 +0200

    Categories refactored to use params

--- Controllers/Categories.php
+++ Controllers/Categories.php
@@ -5,12 +5,13 @@
         return self::query("SELECT `name`, `categoryID` FROM category");
     }
     public function getProductCategory($productID) {
+        $params = array($productID);
         return $this->array_flatten(self::query("SELECT c.name, c.categoryID
         FROM category as c
         INNER JOIN producthascategory as pc
             ON c.categoryID = pc.categoryID
         INNER JOIN product as p
             ON p.productID = pc.productID
-        WHERE pc.productID = ('{$productID}')"));
+        WHERE pc.productID = ? ", $params));
     }
     }
