cc750b89b95dbd90a7ec48c9b616973fe715bef2
nikolaivasilev12 <nikolaivasilev208@gmail.com>
2020-10-27 17:04:41 +0100

    checkoutSuccess page done

--- Views/checkoutSuccess.php
+++ Views/checkoutSuccess.php
@@ -1,15 +1,20 @@
-<h2>SUCESS</h2>
-<?php
-session_start();
-if (isset($_SESSION["cart_item"])) {
-    print_r($cartItem);
-    $cartItem = $_SESSION['cart_item'];
-} else {
-    $cartItem = array();
-}
-if (isset($_POST['checkout'])) {
-    require_once __DIR__ . '/../Controllers/Order.php';
-    $orderObj = new Order();
-    $orderDetails = $_POST;
-    $orderData = $orderObj->saveOrder($orderDetails, $cartItem);
-}
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<div class="container">
+	<div class="row text-center">
+        <div class="col-sm-6 col-sm-offset-3">
+        <br><br> <h2 style="color:#0fad00">Success!</h2>
+        <h3>Your payment was successful!</h3>
+        <p style="font-size:20px;color:#5C5C5C;">Thank you for your purchase. You will recieve an email shortly containing your invoice.</p>
+        </div>
+
+    </div>
+    <div class="row text-center">
+        <a href="index" class="btn btn-primary">
+            Back to our coffee shop
+        </a>
+    </div>
+</div>
