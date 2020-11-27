63c5ca2d921cdafc653217352de324992e6a50ab
nikolaivasilev12 <nikolaivasilev208@gmail.com>
2020-11-28 00:06:57 +0100

    err handler attempt

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 require_once('Routes.php');
 
+function myErrorHandler($errno, $errstr, $errfile, $errline) {
+  echo "<b>Custom error:</b> [$errno] $errstr<br>";
+  echo " Error on line $errline in $errfile<br>";
+}
+
+// Set user-defined error handler function
+set_error_handler("myErrorHandler");
+
 function __autoload($class_name)
 {
   if (file_exists('Classes/' . $class_name . '.php')) {
