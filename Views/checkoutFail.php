c4272248f0bc3e38c80263482b00e771998067af
Simas <simas.raudonis@gmail.com>
2020-11-27 19:21:00 +0200

    Revert "attemp to remove hack files part 2"
    This reverts commit 49246ea7cd45c45f31918ca811c541b156d4ef4a.

--- Views/checkoutFail.php
+++ Views/checkoutFail.php
@@ -4,12 +4,10 @@
 <!------ Include the above in your HEAD tag ---------->
 
 <div class="container">
-    <div class="row text-center">
+	<div class="row text-center">
         <div class="col-sm-6 col-sm-offset-3">
-            <br />
-            <br />
-            <h2 style="color: red;">Transaction failed</h2>
-            <p style="font-size:20px;color:#5C5C5C;">The transaction was declined. Please review your payment details and try again.</p>
+        <br><br> <h2 style="color: red;">Transaction failed</h2>
+        <p style="font-size:20px;color:#5C5C5C;">The transaction was declined. Please review your payment details and try again.</p>
         </div>
     </div>
     <div class="row text-center">
@@ -20,13 +18,6 @@
 </div>
 
 <style>
-    .btn-orange {
-        background-color: #976C42;
-        color: #FFF;
-    }
-
-    .btn-orange:hover {
-        background-color: #49291F;
-        color: #FFF;
-    }
+.btn-orange{background-color:#976C42;color: #FFF;}
+.btn-orange:hover{background-color:#49291F;color: #FFF;}
 </style>
