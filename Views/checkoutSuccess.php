3793c04f3c638cd4aa00db385c5e79e990eea071
nikolaivasilev12 <nikolaivasilev208@gmail.com>
2020-11-27 16:19:41 +0100

    attemp to remove hack files

--- Views/checkoutSuccess.php
+++ Views/checkoutSuccess.php
@@ -4,13 +4,14 @@
 <!------ Include the above in your HEAD tag ---------->
 
 <div class="container">
-	<div class="row text-center">
+    <div class="row text-center">
         <div class="col-sm-6 col-sm-offset-3">
-        <br><br> <h2 style="color:#0fad00">Success!</h2>
-        <h3>Your payment was successful!</h3>
-        <p style="font-size:20px;color:#5C5C5C;">Thank you for your purchase. You will recieve an email shortly containing your invoice.</p>
+            <br />
+            <br />
+            <h2 style="color:#0fad00">Success!</h2>
+            <h3>Your payment was successful!</h3>
+            <p style="font-size:20px;color:#5C5C5C;">Thank you for your purchase. You will recieve an email shortly containing your invoice.</p>
         </div>
-
     </div>
     <div class="row text-center">
         <a href="index" class="btn btn-primary">
