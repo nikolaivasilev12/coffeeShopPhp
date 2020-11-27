d1136a60ae9ed2e1e80b42ed4abdfe17a0faf6dd
nikolaivasilev12 <nikolaivasilev208@gmail.com>
2020-11-28 00:16:25 +0100

    err handler attempt 3

--- index.php
+++ index.php
@@ -1,17 +1,6 @@
 <?php
 require_once('Routes.php');
 
-function handleError($errno, $errstr,$error_file,$error_line) {
-  echo "<b>Error:</b> [$errno] $errstr - $error_file:$error_line";
-  echo "<br />";
-  echo "Terminating PHP Script";
-  
-  die();
-}
-
-// Set user-defined error handler function
-set_error_handler("handleError");
-
 function __autoload($class_name)
 {
   if (file_exists('Classes/' . $class_name . '.php')) {
