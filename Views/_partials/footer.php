20740a182af5746928ba66be6a59cd3677943d27
gunga97 <gunga97@gmail.com>
2020-11-28 20:12:04 +0100

    Cleaned footer code

--- Views/_partials/footer.php
+++ Views/_partials/footer.php
@@ -5,33 +5,33 @@ if (!isset($_SESSION)) {
 ?>
 <footer>
     <div class="container-fluid px-0">
-        <div class="row mx-0" id="background" style="color: #F9F8F0; justify-content: center; align-items: center;">
-        <img id="imageFooter" height="350" class=""
+        <div class="row mx-0" id="background">
+            <img id="imageFooter" height="350" class=""
                 src="data:image/jpg;base64,<?php echo base64_encode(file_get_contents("assets/footer.jpg")); ?>"
             >
-            <div class="col" style="text-align: center; font-weight: 300; margin-left: 5%;">
-                <div style="display: inline-block; text-align: left;">
-                    <h5 style="font-weight: 700; font-family: Marcellus; text-transform: uppercase;">About Us:</h5>
-                    <hr style="margin-left:0;height:2px;width:15%;border-width:2px;background-color:#F9F8F0;">
-                    <p style="color: #F9F8F0; display: inline; line-height: 2;">"<?php echo($index->getCompanyData()['companyDescription']); ?>"</p>
+            <div class="col" style="font-weight: 300; margin-left: 5%;">
+                <div class="content">
+                    <h5>About Us:</h5>
+                    <hr>
+                    <p>"<?php echo($index->getCompanyData()['companyDescription']); ?>"</p>
                 </div>
             </div>
-            <div class="col" style="text-align: center; margin-left: 5%;">
-                <div style="display: inline-block; text-align: left;">
-                    <h5 style="font-weight: 700; font-family: Marcellus; text-transform: uppercase;">Contact Us:</h5>
-                    <hr style="margin-left:0;height:2px;width:15%;border-width:2px;background-color:#F9F8F0;">
-                        <div class="contact" style="color: #9C7738;">   
-                            <p style="color: #F9F8F0; display: inline; line-height: 2;">Address: </p>  <?php echo($index->getCompanyData()['adress']); ?> <br>
-                            <p style="color: #F9F8F0; display: inline; line-height: 2;">Phone:</p>  <?php echo($index->getCompanyData()['phone']); ?> <br>
-                            <p style="color: #F9F8F0; display: inline; line-height: 2;">Email: </p> <?php echo($index->getCompanyData()['email']); ?>
+            <div class="col">
+                <div class="content">
+                    <h5>Contact Us:</h5>
+                    <hr>
+                        <div class="contact">   
+                            <p>Address: </p>  <?php echo($index->getCompanyData()['adress']); ?> <br>
+                            <p>Phone:</p>  <?php echo($index->getCompanyData()['phone']); ?> <br>
+                            <p>Email: </p> <?php echo($index->getCompanyData()['email']); ?>
                         </div>
                 </div>
             </div>
-            <div class="col" style="text-align: center; margin-right: 5%;">
-                <div style="display: inline-block; text-align: left;">
-                    <h5 style="font-weight: 700; font-family: Marcellus; text-transform: uppercase;">Opening Hours:</h5>
-                    <hr style="margin-left:0;height:2px;width:15%;border-width:2px;background-color:#F9F8F0;">
-                        <div class="hours" style="color: #F9F8F0; line-height: 1;">
+            <div class="col">
+                <div class="content">
+                    <h5>Opening Hours:</h5>
+                    <hr>
+                        <div class="hours">
                             <?php
                             foreach ($index->getWorkdays() as $key => $value) {
                             ?>
@@ -57,12 +57,39 @@ if (!isset($_SESSION)) {
             <p>© Copyright 2020 Coffee Shop. All rights Reserved. Designed by Niko, Simas & Ugne</p>
         </div>
 <style lang="css">
+p {
+    color: #F9F8F0; display: inline; line-height: 2; 
+}
+hr {
+    margin-left:0;
+    height:2px;
+    width:15%;
+    border-width:2px;
+    background-color:#F9F8F0;  
+}
+h5 {
+    font-weight: 700; font-family: Marcellus; text-transform: uppercase;
+}
+.row {
+    color: #F9F8F0; justify-content: center; align-items: center;
+}
+col {
+    text-align: center; margin-left: 5%; font-weight: 300;
+}
+.content {
+    display: inline-block; text-align: left;
+}
 #imageFooter {
     position: absolute;
     height: 100% !important;
     width: 100% !important; /* IT WORKS!!!!!!!! */
 }
-
+.contact {
+    color: #9C7738;
+}
+.hours {
+    color: #F9F8F0; line-height: 1;
+}
 #background {
     position: absolute;
     height: 380px; /* You must set a specified height */
@@ -76,5 +103,4 @@ if (!isset($_SESSION)) {
     grid-template-columns: 20% 30% 50%; 
     margin-left: -5%; 
 }
-
 </style>
